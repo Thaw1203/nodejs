36d5389973fa5e1a5e9f19dfb253a43694d3213d
Thaw1203 <thawane0307@gmail.com>
2025-10-06 09:10:40 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+//console.log("teste está funcionando");
+
+console.log("inicio...");
+
+setTimeout(()=>{
+    console.log("Esperou 2 segundos");
+}, 2000);
+
+console.log("Fim...");
+
+const somar = require('./somar');
+console.log(somar(5,3));
